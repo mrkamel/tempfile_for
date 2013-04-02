30189c3827dd3e63a40a08addc3c37eaf7a5444e
Benjamin Vetter <vetter@plainpicture.de>
2013-04-02 21:44:44 +0200

    Preserve encoding when on ruby >= 1.9

--- lib/tempfile_for.rb
+++ lib/tempfile_for.rb
@@ -4,7 +4,13 @@ require "tempfile"
 
 class Tempfile
   def self.for(data)
-    tempfile = open("tempfile")
+    tempfile = nil
+
+    if RUBY_VERSION < "1.9"
+      tempfile = open("tempfile")
+    else
+      tempfile = open("tempfile", :encoding => data.encoding)
+    end
 
     begin
       tempfile.write data
@@ -14,7 +20,11 @@ class Tempfile
 
       tempfile.flush
 
-      File.read(tempfile.path)
+      if RUBY_VERSION < "1.9"
+        File.read tempfile.path
+      else
+        File.read tempfile.path, :encoding => data.encoding
+      end
     ensure
       tempfile.close!
     end
--- test/tempfile_for_test.rb
+++ test/tempfile_for_test.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 
 require File.expand_path("../../lib/tempfile_for", __FILE__)
 require "test/unit"
@@ -5,6 +6,12 @@ require "test/unit"
 class TempfileForTest < Test::Unit::TestCase
   def test_tempfile_for
     assert_equal "string1, string2", Tempfile.for("string1") { |tempfile| `echo -n ', string2' >> #{tempfile.path}` }
+
+    if RUBY_VERSION >= "1.9"
+      string = "string1".force_encoding("ISO-8859-1")
+      res = Tempfile.for(string) { |tempfile| `echo -n ', string2' >> #{tempfile.path}` }
+      assert_equal "ISO-8859-1", res.encoding.name
+    end
   end
 end
 
